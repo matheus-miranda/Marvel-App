bd0507a5fc72c964e6b34c583e4606d34adb40b4
matheus-miranda <matheus.sm86@gmail.com>
2022-02-05 22:03:19 -0300

    [TEST] - Create test for GetCharactersUseCaseImpl

--- core/src/main/java/br/com/msmlabs/core/usecase/GetCharactersUseCase.kt
+++ core/src/main/java/br/com/msmlabs/core/usecase/GetCharactersUseCase.kt
@@ -21,8 +21,9 @@ class GetCharactersUseCaseImpl @Inject constructor(
 ) : PagingUseCase<GetCharactersParams, Character>(), GetCharactersUseCase {
 
     override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
+        val pagingSource = charactersRepository.getCharacters(params.query)
         return Pager(config = params.pagingConfig) {
-            charactersRepository.getCharacters(params.query)
+            pagingSource
         }.flow
     }
 }
