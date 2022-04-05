ea0f29c88a8398a3c1e985f538086536a000e993
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-04 22:00:21 -0300

    [FEATURE] - Create Detail Fragment with animation transition

---
+++ app/src/main/java/com/example/marvelapp/presentation/detail/DetailFragment.kt
@@ -0,0 +1,55 @@
+package com.example.marvelapp.presentation.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.transition.TransitionInflater
+import com.bumptech.glide.Glide
+import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val args by navArgs<DetailFragmentArgs>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentDetailBinding.inflate(inflater, container, false).apply {
+        _binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val detailViewArg = args.detailsViewArgs
+        binding.ivCharacter.run {
+            transitionName = detailViewArg.name
+            Glide.with(context)
+                .load(detailViewArg.imageUrl)
+                .fallback(R.drawable.ic_img_loading_error)
+                .into(this)
+        }
+        setSharedElementTransitionOnEnter()
+    }
+
+    // Define transition animation as "move"
+    private fun setSharedElementTransitionOnEnter() {
+        TransitionInflater.from(requireContext())
+            .inflateTransition(android.R.transition.move).apply {
+                sharedElementEnterTransition = this
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
