fef880a6a56fe3c819178b7c736d4f174e082c60
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-12 21:25:16 -0300

    [FIX] - Create and implement fetch comics response

--- app/src/main/java/com/example/marvelapp/framework/remote/RetrofitCharactersDataSource.kt
+++ app/src/main/java/com/example/marvelapp/framework/remote/RetrofitCharactersDataSource.kt
@@ -2,8 +2,10 @@ package com.example.marvelapp.framework.remote
 
 import br.com.msmlabs.core.data.repository.CharactersRemoteDataSource
 import br.com.msmlabs.core.domain.model.CharacterPaging
+import br.com.msmlabs.core.domain.model.Comics
 import com.example.marvelapp.framework.network.MarvelApi
 import com.example.marvelapp.framework.network.response.toCharacterModel
+import com.example.marvelapp.framework.network.response.toComicModel
 import javax.inject.Inject
 
 class RetrofitCharactersDataSource @Inject constructor(
@@ -16,5 +18,11 @@ class RetrofitCharactersDataSource @Inject constructor(
             it.toCharacterModel()
         }
         return CharacterPaging(data.offset, data.total, characters)
+    }
+
+    override suspend fun fetchComics(characterId: Int): List<Comics> {
+        return marvelApi.getComics(characterId).data.results.map {
+            it.toComicModel()
+        }
     }
 }
--- core/src/main/java/br/com/msmlabs/core/data/repository/CharactersRemoteDataSource.kt
+++ core/src/main/java/br/com/msmlabs/core/data/repository/CharactersRemoteDataSource.kt
@@ -1,7 +1,10 @@
 package br.com.msmlabs.core.data.repository
 
 import br.com.msmlabs.core.domain.model.CharacterPaging
+import br.com.msmlabs.core.domain.model.Comics
 
 interface CharactersRemoteDataSource {
     suspend fun fetchCharacters(queries: Map<String, String>): CharacterPaging
+
+    suspend fun fetchComics(characterId: Int): List<Comics>
 }
