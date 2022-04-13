58fceefcfed7f112af042161077a815820756301
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-12 21:23:54 -0300

    [FIX] - Adjust API return type

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -4,7 +4,7 @@ import br.com.msmlabs.core.domain.model.Character
 import com.google.gson.annotations.SerializedName
 
 data class CharacterResponse(
-    @SerializedName("id") val id: String,
+    @SerializedName("id") val id: Int,
     @SerializedName("name") val name: String,
     @SerializedName("thumbnail") val thumbnail: ThumbnailResponse
 )
