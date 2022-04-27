e33914f86d944c1c930b030aaf50a92d04b3e4c1
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-26 23:04:09 -0300

    [FEAT] - Create FavoriteDao

---
+++ app/src/main/java/com/example/marvelapp/framework/db/dao/FavoriteDao.kt
@@ -0,0 +1,19 @@
+package com.example.marvelapp.framework.db.dao
+
+import androidx.room.*
+import br.com.msmlabs.core.data.DbConstants
+import com.example.marvelapp.framework.db.entity.FavoriteEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+
+    @Query("SELECT * FROM ${DbConstants.FAVORITES_TABLE_NAME}")
+    fun loadFavorites(): Flow<List<FavoriteEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favoriteEntity: FavoriteEntity)
+
+    @Delete
+    suspend fun deleteFavorite(favoriteEntity: FavoriteEntity)
+}
