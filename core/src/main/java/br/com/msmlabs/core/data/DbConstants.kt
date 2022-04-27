4a65906cdd121b3e17efcc8833d4fb6fc40276c0
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-27 20:58:41 -0300

    [FEAT] - Create AppDatabase and DI module

--- core/src/main/java/br/com/msmlabs/core/data/DbConstants.kt
+++ core/src/main/java/br/com/msmlabs/core/data/DbConstants.kt
@@ -1,6 +1,7 @@
 package br.com.msmlabs.core.data
 
 object DbConstants {
+    const val APP_DATABASE_NAME = "app_database.db"
     const val FAVORITES_TABLE_NAME = "favorites"
     const val FAVORITES_COLUMN_INFO_ID = "id"
     const val FAVORITES_COLUMN_INFO_NAME = "name"
