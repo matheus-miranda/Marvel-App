4c7db9646b02a0aa4f233a92f8fff4016b9bcfcf
Matheus Miranda <matheus.sm86@gmail.com>
2022-04-27 21:31:14 -0300

    [FEAT] - Create FavoritesRepository, implementation and local data source

---
+++ app/src/main/java/com/example/marvelapp/framework/FavoritesRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.example.marvelapp.framework
+
+import br.com.msmlabs.core.data.repository.FavoritesLocalDataSource
+import br.com.msmlabs.core.data.repository.FavoritesRepository
+import br.com.msmlabs.core.domain.model.Character
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class FavoritesRepositoryImpl @Inject constructor(
+    private val favoritesLocalDataSource: FavoritesLocalDataSource
+) : FavoritesRepository {
+
+    override fun getAll(): Flow<List<Character>> {
+        return favoritesLocalDataSource.fetchAll()
+    }
+
+    override suspend fun saveFavorite(character: Character) {
+        favoritesLocalDataSource.save(character)
+    }
+
+    override suspend fun deleteFavorite(character: Character) {
+        favoritesLocalDataSource.delete(character)
+    }
+}
---
+++ core/src/main/java/br/com/msmlabs/core/data/repository/FavoritesRepository.kt
@@ -0,0 +1,10 @@
+package br.com.msmlabs.core.data.repository
+
+import br.com.msmlabs.core.domain.model.Character
+import kotlinx.coroutines.flow.Flow
+
+interface FavoritesRepository {
+    fun getAll(): Flow<List<Character>>
+    suspend fun saveFavorite(character: Character)
+    suspend fun deleteFavorite(character: Character)
+}
