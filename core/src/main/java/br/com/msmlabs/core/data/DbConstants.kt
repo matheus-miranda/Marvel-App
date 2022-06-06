c994413aeeab0c800503143905b989a4011d89ad
Matheus Miranda <matheus.sm86@gmail.com>
2022-06-05 21:11:25 -0300

    [FEAT] - Create RemoteKey Entity and DAO

--- core/src/main/java/br/com/msmlabs/core/data/DbConstants.kt
+++ core/src/main/java/br/com/msmlabs/core/data/DbConstants.kt
@@ -10,4 +10,6 @@ object DbConstants {
     const val CHARACTERS_COLUMN_INFO_ID = "id"
     const val CHARACTERS_COLUMN_INFO_NAME = "name"
     const val CHARACTERS_COLUMN_INFO_IMAGE_URL = "image_url"
+    const val REMOTE_KEYS_TABLE_NAME = "remote_keys"
+    const val REMOTE_KEYS_COLUMN_INFO_OFFSET = "next_offset"
 }
